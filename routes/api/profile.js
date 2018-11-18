4e220d2d19bb0ca1caafb0560496b7268b2e3ee8
edgar_montano <em2532@nyu.edu>
2018-11-18 17:53:26 -0500

    added profile post router and validation for page

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,5 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
+
+// load models
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
+
+//validation import
+const validateProfileInupt = require("../../validation/profile");
 
 // @route       GET api/profile/test
 // @desc        Test post route
@@ -8,6 +17,90 @@ router.get("/test", (req, res) =>
   res.json({
     msg: "Profile works"
   })
+);
+
+// @route       GET api/profile
+// @desc        Get current users profile
+// @access      private
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const errors = {};
+    Profile.findOne({ user: req.user.id })
+      .populate("user", ["name", "avatar"])
+      .then(profile => {
+        if (!profile) {
+          errors.noprofile = "There is no profile for this user";
+          return res.status(404).json(errors);
+        } else {
+          res.json(profile);
+        }
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
+
+// @route       POST api/profile
+// @desc        Create or edit user profile
+// @access      private
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    //Validate input and respond with errors
+    const { errors, isValid } = validateProfileInupt(req.body);
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+
+    // Get fields for profile
+    const profileFields = {};
+    profileFields.user = req.user.id;
+
+    //checking to see if sent from form
+    if (req.body.handle) profileFields.handle = req.body.handle;
+    if (req.body.company) profileFields.company = req.body.company;
+    if (req.body.website) profileFields.website = req.body.website;
+    if (req.body.location) profileFields.location = req.body.location;
+    if (req.body.bio) profileFields.bio = req.body.bio;
+    if (req.body.status) profileFields.status = req.body.status;
+    if (req.body.githubusername)
+      profileFields.githubusername = req.body.githubusername;
+    // Array of skills
+    if (typeof req.body.skills !== "undefined") {
+      profileFields.skills = req.body.skills.split(",");
+    }
+    // social fields
+    profileFields.social = {}; // must initialize social field before adding to it
+    if (req.body.youtube) profileFields.social.youtube = req.body.youtube;
+    if (req.body.twitter) profileFields.social.twitter = req.body.twitter;
+    if (req.body.linkedin) profileFields.social.linkedin = req.body.linkedin;
+    if (req.body.instagram) profileFields.social.instagram = req.body.instagram;
+
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      //this means that this is a profile update not a profile create request
+      if (profile) {
+        Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true }
+        ).then(profile => res.json(profile));
+      }
+      //this means its a create
+      else {
+        //check if handle exists
+        Profile.findOne({ handle: profileFields.handle }).then(profile => {
+          if (profile) {
+            errors.handle = "Handle already exists";
+            res.status(400).json(errors);
+          }
+          //save profile
+          new Profile(profileFields).save().then(profile => res.json(profile));
+        });
+      }
+    });
+  }
 );
 
 module.exports = router;
